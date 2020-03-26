87de1f4eb7a1ab078fae786ebf6c2dcfcab6435b
Casper Kuijjer <casper@kuijjer.com>
2020-03-26 10:38:24 +0100

    example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useContext, createContext, useReducer } from 'react';
 import './App.css';
 
-function App() {
+const AppContext = createContext({});
+
+const reducer = (state, action) => {
+  let nextState = null;
+
+  switch (action.type) {
+    case 'setA':
+      nextState = { a: action.value, b: state.b, c: action.value + state.b };
+      break;
+    case 'setB':
+      nextState = { a: state.a, b: action.value, c: state.a + action.value };
+      break;
+    case 'log': {
+      console.log('logUsingReducer', state.c);
+      nextState = state;
+      break;
+    }
+    default:
+      return state;
+  }
+
+  console.log('reducer called', { state, action, nextState });
+
+  return nextState;
+};
+
+const AppContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, { a: 0, b: 0, c: 0 });
+
+  const setA = value => dispatch({ type: 'setA', value });
+  const setB = value => dispatch({ type: 'setB', value });
+  const logUsingReducer = () => dispatch({ type: 'log' });
+
+  const getState = () => {
+    return state;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={{ ...state, setA, setB, getState, logUsingReducer }}>{children}</AppContext.Provider>
   );
-}
+};
+
+const useCLogger = () => {
+  const { c, getState } = useContext(AppContext);
+
+  const log = () => console.log('useCLogger', { c, getState: getState().c });
+  return log;
+};
+
+const InputA = () => {
+  console.log('render InputA');
+  const { a, setA } = useContext(AppContext);
+  const log = useCLogger();
+
+  const onChange = e => {
+    setA(+e.target.value);
+    log();
+  };
+
+  return <input type="number" value={a} onChange={onChange} />;
+};
+
+const InputB = () => {
+  console.log('render InputB');
+  const { b, setB, logUsingReducer } = useContext(AppContext);
+  const log = useCLogger();
+
+  const onChange = e => {
+    setB(+e.target.value);
+    logUsingReducer();
+    log();
+  };
+
+  return <input type="number" value={b} onChange={onChange} />;
+};
+
+const InputC = () => {
+  console.log('render InputC');
+  const { c } = useContext(AppContext);
+
+  return <input type="number" value={c} readOnly />;
+};
+
+const App = () => (
+  <div className="App">
+    <AppContextProvider>
+      <InputA />+
+      <InputB />=
+      <InputC />
+    </AppContextProvider>
+  </div>
+);
 
 export default App;
